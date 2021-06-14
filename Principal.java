37dbc9e0a4416694f6e92c07a761148962a1eb55
akiofabio <fabio.ikenoue@fatec.sp.gov.br>
2021-06-13 21:09:07 -0300

    Update codigo
    Adicionado as classes Pedido e GerenciamentoDePedido

--- Principal.java
+++ Principal.java
@@ -1,3 +1,8 @@
+
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -8,9 +13,9 @@ public class Principal {
         //Cadastro dos Ingredientes
         System.out.println("Cadastrando Ingredientes");
         ingrediente.setNome("Queijo Mussarela");
-        ingrediente.setQuantidadePorPorção(300);
-        ingrediente.setCaloriasPorPorção(840);
-        ingrediente.setPrecoPorPorção(12);
+        ingrediente.setQuantidadePorPorcao(300);
+        ingrediente.setCaloriasPorPorcao(840);
+        ingrediente.setPrecoPorPorcao(12);
         if(cardapio.addIngrediente(ingrediente))
             System.out.println("    Igrediente "+ingrediente.getNome()+ " cadastrado com sucesso!!");
         else
@@ -18,9 +23,9 @@ public class Principal {
             
         ingrediente = new Ingrediente();
         ingrediente.setNome("Tomate");
-        ingrediente.setQuantidadePorPorção(100);
-        ingrediente.setCaloriasPorPorção(18);
-        ingrediente.setPrecoPorPorção(0.6);
+        ingrediente.setQuantidadePorPorcao(100);
+        ingrediente.setCaloriasPorPorcao(18);
+        ingrediente.setPrecoPorPorcao(0.6);
         if(cardapio.addIngrediente(ingrediente))
             System.out.println("    Igrediente "+ingrediente.getNome()+ " cadastrado com sucesso!!");
         else
@@ -28,9 +33,9 @@ public class Principal {
         
         ingrediente = new Ingrediente();
         ingrediente.setNome("Molho de Tomate");
-        ingrediente.setQuantidadePorPorção(50);
-        ingrediente.setCaloriasPorPorção(15);
-        ingrediente.setPrecoPorPorção(0.4);
+        ingrediente.setQuantidadePorPorcao(50);
+        ingrediente.setCaloriasPorPorcao(15);
+        ingrediente.setPrecoPorPorcao(0.4);
         if(cardapio.addIngrediente(ingrediente))
             System.out.println("    Ingrediente "+ingrediente.getNome()+ " cadastrado com sucesso!!");
         else
@@ -38,9 +43,9 @@ public class Principal {
         
         ingrediente = new Ingrediente();
         ingrediente.setNome("Linguiça Calabresa");
-        ingrediente.setQuantidadePorPorção(350);
-        ingrediente.setCaloriasPorPorção(1050);
-        ingrediente.setPrecoPorPorção(7);
+        ingrediente.setQuantidadePorPorcao(350);
+        ingrediente.setCaloriasPorPorcao(1050);
+        ingrediente.setPrecoPorPorcao(7);
         if(cardapio.addIngrediente(ingrediente))
             System.out.println("    Ingrediente "+ingrediente.getNome()+ " cadastrado com sucesso!!");
         else
@@ -48,9 +53,9 @@ public class Principal {
         
         ingrediente = new Ingrediente();
         ingrediente.setNome("Cebola");
-        ingrediente.setQuantidadePorPorção(150);
-        ingrediente.setCaloriasPorPorção(60);
-        ingrediente.setPrecoPorPorção(0.6);
+        ingrediente.setQuantidadePorPorcao(150);
+        ingrediente.setCaloriasPorPorcao(60);
+        ingrediente.setPrecoPorPorcao(0.6);
         if(cardapio.addIngrediente(ingrediente))
             System.out.println("    Ingrediente "+ingrediente.getNome()+ " cadastrado com sucesso!!");
         else
@@ -88,9 +93,9 @@ public class Principal {
         System.out.println("Removendo Ingrediente");
         ingrediente = new Ingrediente();
         ingrediente.setNome("Queijo Branco");
-        ingrediente.setQuantidadePorPorção(150);
-        ingrediente.setCaloriasPorPorção(60);
-        ingrediente.setPrecoPorPorção(0.6);
+        ingrediente.setQuantidadePorPorcao(150);
+        ingrediente.setCaloriasPorPorcao(60);
+        ingrediente.setPrecoPorPorcao(0.6);
         if(cardapio.addIngrediente(ingrediente))
             System.out.println("    Ingrediente "+ingrediente.getNome()+ " cadastrado com sucesso!!");
         else
@@ -120,6 +125,7 @@ public class Principal {
         p1.addIgredientes(cardapio.consultarIngrediente("Queijo Muçarela"));
         p1.addIgredientes(cardapio.consultarIngrediente("Molho de Tomate"));
         p1.addIgredientes(cardapio.consultarIngrediente("Tomate"));
+        p1.setPreco(30);
         if(cardapio.addPizza(p1))
             System.out.println("    Pizza "+ p1.getNome() +" cadastrada com sucesso!!");
         else
@@ -131,6 +137,7 @@ public class Principal {
         p1.addIgredientes(cardapio.consultarIngrediente("Molho de Tomate"));
         p1.addIgredientes(cardapio.consultarIngrediente("Cebola"));
         p1.addIgredientes(cardapio.consultarIngrediente("Linguiça Calabresa"));
+        p1.setPreco(35);
         if(cardapio.addPizza(p1))
             System.out.println("    Pizza "+ p1.getNome() +" cadastrada com sucesso!!");
         else
@@ -234,5 +241,132 @@ public class Principal {
             System.out.println("    Pizza "+ p1.getNome() +" encontrada com sucesso!!");
         else
             System.out.println("    Pizza Calabresa II não encontrada!!");
+        
+        //Realização de Pedidos
+        System.out.println("\nRealização de Pedidos");
+        GerenciadorDePedido gerenPedidos = new GerenciadorDePedido();
+        Pedido pedido = new Pedido();
+        
+        pedido.setNome("Ana");
+        pedido.setEndereco("Na casa dela");
+        pedido.setTelefone("123456789");
+        pedido.addPizza(cardapio.consultarPizza("Muçarela"));
+        pedido.addPizza(cardapio.consultarPizza("Muçarela"));
+        pedido.setTaxaDeEntrega(5.5);
+        pedido.setTipoDePagamento("Cartão de Debito");
+        pedido.fecharPedido();
+        
+        if(gerenPedidos.addPedido(pedido)){
+            System.out.println("    Pedido realizado com sucesso!!");
+            mostrarPedido(pedido);
+        }
+        else
+            System.out.println("    Pedido de "+ pedido.getNome() +" não realizado com sucesso!!");
+        
+        pedido = new Pedido();
+        pedido.setNome("Daniel");
+        pedido.setEndereco("Na casa dele");
+        pedido.setTelefone("987654321");
+        pedido.addPizza(cardapio.consultarPizza("Muçarela"));
+        pedido.addPizza(cardapio.consultarPizza("Calabresa"));
+        pedido.setTaxaDeEntrega(5.5);
+        pedido.setTipoDePagamento("Cartão de Credito");
+        pedido.fecharPedido();
+        
+        if(gerenPedidos.addPedido(pedido)){
+            System.out.println("    Pedido realizado com sucesso!!");
+            mostrarPedido(pedido);
+        }
+        else
+            System.out.println("    Pedido de "+ pedido.getNome() +" não realizado com sucesso!!");
+        
+        pedido = new Pedido();
+        pedido.setNome("Fabio");
+        pedido.setEndereco("Na Pizzaria");
+        pedido.setTelefone("543219876");
+        pedido.addPizza(cardapio.consultarPizza("Calabresa"));
+        pedido.setTaxaDeEntrega(0);
+        pedido.setTipoDePagamento("Dinheiro");
+        pedido.fecharPedido();
+        
+        if(gerenPedidos.addPedido(pedido)){
+            System.out.println("    Pedido realizado com sucesso!!");
+            mostrarPedido(pedido);
+        }
+        else
+            System.out.println("    Pedido de "+ pedido.getNome() +" não realizado com sucesso!!");
+        
+        //Alterar pedido
+        System.out.println("Alterar pedido");
+        
+        pedido = gerenPedidos.consultarUltimoPedido("123456789");
+        if(pedido!=null){
+            System.out.println("    Pedido encontrado com sucesso!!");
+            mostrarPedido(pedido);
+        }
+        else
+            System.out.println("    Pedido encontrado não com sucesso!!");
+        
+        p1 = pedido.consultarPizza("Muçarela");
+        if(pedido.removePizza(p1)){
+            System.out.println("    Pizza Removida com sucesso!!");
+            pedido.fecharPedido();
+            mostrarPedido(pedido);
+        }
+        else{
+            System.out.println("    Pizza Removida não com sucesso!!");
+        }
+        
+        if(gerenPedidos.alterarPedido(pedido)){
+            System.out.println("    Pedido alterado com sucesso!!");
+        }
+        else
+            System.out.println("    Pedido não alterado com sucesso!!");
+        
+        //Remover/Cancelar pedido
+        System.out.println("Remover/Cancelar pedido");
+        
+        pedido = gerenPedidos.consultarUltimoPedido("987654321");
+        if(pedido!=null){
+            System.out.println("    Pedido encontrado com sucesso!!");
+            mostrarPedido(pedido);
+        }
+        else
+            System.out.println("    Pedido encontrado não com sucesso!!");
+                
+        if(gerenPedidos.removePedido(pedido)){
+            System.out.println("    Pedido removido com sucesso!!");
+        }
+        else
+            System.out.println("    Pedido não removido com sucesso!!");
+        
+        //todos os pedidos
+        System.out.println("Todos os pedidos");
+        ArrayList<Pedido> pedidos;
+        pedidos = gerenPedidos.getTodosPedido();
+        for(Pedido pe : pedidos){
+            mostrarPedido(pe);
+            System.out.println("--------------------------------------");
+        }
+    
+        
     }
+    
+    static void mostrarPedido(Pedido pedido){
+        System.out.println("        Data: "+ pedido.getData());
+        System.out.println("        Nome: "+ pedido.getNome());
+        System.out.println("        Endereço: "+ pedido.getEndereco());
+        System.out.println("        Telefone: "+ pedido.getTelefone());
+        System.out.print("        Pedido: ");
+        for(Pizza pizza: pedido.getTodosPizzas())
+            System.out.print(pizza.getNome()+"; ");
+        System.out.println();
+        System.out.println("        Tipo de Pagamento: "+ pedido.getTipoDePagamento());
+        System.out.println("        Taxa de Entrega: "+ pedido.getTaxaDeEntrega());
+        System.out.println("        Preço das Pizzas: "+ pedido.getPrecoPizzas());
+        System.out.println("        Total: "+ pedido.getPrecoTotal());
+        
+    }
+    
+    
 }
